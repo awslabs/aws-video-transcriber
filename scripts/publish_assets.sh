4eefe9933d5ee70d8d0bcfbfe81b1c112a430c3e
Josh Passenger <jospas@amazon.com>
2020-02-10 19:43:26 +1000

    Added new code SHA for CustomLambda and removed layer deployment

--- scripts/publish_assets.sh
+++ scripts/publish_assets.sh
@@ -16,9 +16,5 @@ cd ../web
 
 aws s3 cp --recursive . s3://aws-captions-deployment-ap-southeast-2/web/ --profile transcribe --acl public-read --exclude ".DS_Store"
 
-cd ../layers/.serverless
 
-aws s3 cp aws-captions-node-sdk-layer.zip s3://aws-captions-deployment-ap-southeast-2/layers/ --profile transcribe --acl public-read
-
-cd ../../scripts
 
